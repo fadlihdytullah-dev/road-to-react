b039665a3dc6f83dd9ef57c32b70182d8b45611e
Fadli Hidayatullah <fadli.hidayatullah@kumparan.com>
2020-03-13 11:57:17 +0700

    react jsx
    Signed-off-by: Fadli Hidayatullah <fadli.hidayatullah@kumparan.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './FlexboxGrid.css';
+
+const appInfo = {
+  title: 'HackerStories',
+  subtitle: 'Hack Your Life and Make it Better!',
+  createdYear: 2018,
+};
+
+const topics = [
+  'JavaScript',
+  'React',
+  'Node',
+  'Vue',
+  'Gatsby',
+  'NextJS',
+  'CSS',
+];
+
+function getCurrentYear() {
+  return new Date().getFullYear;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrap container-fluid mt-2">
+      <h1>{appInfo.title}</h1>
+      <h3 className="nobold">{appInfo.subtitle}</h3>
+
+      <div className="form-control">
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" className="text-input" />
+      </div>
+
+      <section id="topics" className="mb-4">
+        <h3 className="mb-2">Topics</h3>
+        {topics.map(topic => (
+          <span key={topic} className="inline-block p-1">
+            <span className="border border-rounded p-1">{topic}</span>
+          </span>
+        ))}
+      </section>
+
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This project created at {appInfo.createdYear} and now{' '}
+          {getCurrentYear()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
   );
 }
