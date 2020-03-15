9be0126f5d24965455c08313f2a791307a8714a7
Fadli Hidayatullah <fadlihdytullah.dev@gmail.com>
2020-03-15 18:44:10 +0700

    Async Await
    Signed-off-by: Fadli Hidayatullah <fadlihdytullah.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -116,18 +116,18 @@ const App = () => {
   const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
   // This will be differ when the saerchTerm changed, run the side effect
-  const handleFetchStories = React.useCallback(() => {
+  const handleFetchStories = React.useCallback(async () => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
+    try {
+      const result = await axios.get(url);
 
-    axios
-      .get(url)
-      .then(result => {
-        dispatchStories({
-          type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.hits,
-        });
-      })
-      .catch(() => dispatchStories({type: 'STORIES_FETCH_FAILURE'}));
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch (error) {
+      dispatchStories({type: 'STORIES_FETCH_FAILURE'});
+    }
   }, [url]);
 
   React.useEffect(() => {
